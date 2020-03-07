992d805b89ce20a11858d79748499799efc7866f
MINboooo <minminnotsemi1203@hotmail.co.jp>
2020-03-07 21:18:38 +0900

    additional

---
+++ app/controllers/calcs_controller.rb
@@ -0,0 +1,20 @@
+class CalcsController < ApplicationController
+  def show
+    alue1 = params[:value1].to_i
+    calc = params[:calc]
+    value2 = params[:value2].to_i
+    
+    @result = case calc
+              when "addition"
+                value1 + value2
+              when "subtraction"
+                @result = value1 - value2
+              when "multiplication"
+                @result = value1 * value2
+              when "division"
+                @result = value1 / value2
+              else
+                @result = "誤字！"
+    end
+  end
+end
